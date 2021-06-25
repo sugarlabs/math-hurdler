b8d7a8f710bcbe85f5c05d299af874fa96a690bb
sourabhaa <sourabhayoga@gmail.com>
2021-06-25 23:28:12 +0530

    flake8 fixes

--- objects/button.py
+++ objects/button.py
@@ -1,13 +1,14 @@
-
 import pygame
 
+
 class Button(object):
-    def __init__(self, text, font, font_color, height, width, color, border_color=None, border_thickness=None):
+    def __init__(self, text, font, font_color, height, width,
+                 color, border_color=None, border_thickness=None):
         self.height = height
         self.width = width
         self.border_color = border_color
         self.border_thickness = border_thickness
-        self.image = pygame.Surface((self.height,self.width))
+        self.image = pygame.Surface((self.height, self.width))
         self.rect = self.image.get_rect()
         self.set_color(color)
         self.font = font
@@ -15,9 +16,11 @@ class Button(object):
         self.set_text(text)
 
     def draw(self, surface):
-        self.label_rect.center = (self.rect.x+self.image.get_width()/2, self.rect.y+self.image.get_height()/2)
-        surface.blit(self.image,self.rect)
-        surface.blit(self.label,self.label_rect)
+        self.label_rect.center = (
+            self.rect.x + self.image.get_width() / 2,
+            self.rect.y + self.image.get_height() / 2)
+        surface.blit(self.image, self.rect)
+        surface.blit(self.label, self.label_rect)
 
     def mouse_click(self, mouse, action, *args):
         if self.rect.collidepoint(mouse):
@@ -25,19 +28,21 @@ class Button(object):
 
     def set_text(self, text):
         self.text = text
-        self.label = self.font.render(self.text,1,self.font_color)
+        self.label = self.font.render(self.text, 1, self.font_color)
         self.label_rect = self.label.get_rect()
 
     def set_color(self, color, save_color=True):
         if save_color:
             self.color = color
             self.selected_color = (color[0] * .8, color[1] * .8, color[2] * .8)
-            
+
         if self.border_thickness and self.border_color:
             self.image.fill(self.border_color)
             self.rect = self.image.get_rect()
-            box = self.rect.inflate(self.border_thickness,self.border_thickness)
-            self.image.fill(color,box)
+            box = self.rect.inflate(
+                self.border_thickness,
+                self.border_thickness)
+            self.image.fill(color, box)
         else:
             self.image.fill(color)
 
